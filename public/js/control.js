8105d0eb47b9aea9ad711ff7e204eefd12db3c19
Gopi Suresh <suresh.gopikrishnan@gmail.com>
2018-01-11 21:21:58 -0800

    Merged new

--- public/js/control.js
+++ public/js/control.js
@@ -59,6 +59,9 @@ function submitProgram(){
 }
 
 function leftField(){
+    clearInterval(tickInterval);
+    var button = document.getElementById("runButton");
+    button.innerHTML = "Run";
     field_index = (field_index == 0)?fields.length-1: field_index-1;
     sw = new StudentWorld();
     sw.init(compiledProgram,cpuProgram,cpuProgram,cpuProgram);
@@ -66,8 +69,41 @@ function leftField(){
 }
 
 function rightField(){
+    clearInterval(tickInterval);
+    var button = document.getElementById("runButton");
+    button.innerHTML = "Run";
     field_index++;
     sw = new StudentWorld();
     sw.init(compiledProgram,cpuProgram,cpuProgram,cpuProgram);
     sw.draw();
 }
+
+function addField(evt){
+    console.log(evt.target.files);
+    var reader  = new FileReader();
+
+    reader.addEventListener("load", function () {
+        clearInterval(tickInterval);
+        var button = document.getElementById("runButton");
+        button.innerHTML = "Run";
+        var f = atob(reader.result.substring(23));
+        var fl = "";
+        for(var i = 0; i < f.length; i++){
+            if(f[i] != "\n")
+                fl += f[i];
+        }
+        fields.push(fl);
+        field_index = fields.length-1;
+        sw = new StudentWorld();
+        sw.init(compiledProgram,cpuProgram,cpuProgram,cpuProgram);
+        sw.draw();
+        // var abs = reader.result;
+        // console.log(reader.result);
+        // console.log(btoa(reader.result));
+    }, false);
+
+    if (file) {
+        reader.readAsDataURL(evt.target.files[0]);
+    }
+}
+document.getElementById("file").addEventListener('change', addField, false);
